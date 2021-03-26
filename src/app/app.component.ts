d61db55fb5874744e00a05ce8c579ee68cff2946
akashmohanlal <mohanlal.akash@gmail.com>
2021-03-26 17:34:22 +0000

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'mw-app',
+  // templateUrl: './app.component.html',
+  template: `<h1>This is my first app</h1>
+  <h2>The app is a message reader with a form</h2>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angulartest';
+  title = 'welcome to angular';
 }
