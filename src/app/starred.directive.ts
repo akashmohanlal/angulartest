b53cd62562a034a2307a376dbe2bf77cc3ab3218
akashmohanlal <akash.mohanlal@ert.com>
2021-03-31 15:45:00 +0100

    added starred directive, bug fixing

--- src/app/starred.directive.ts
+++ src/app/starred.directive.ts
@@ -1,10 +1,17 @@
-import { Directive, HostBinding } from '@angular/core';
+import { Directive, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[atStarred]'
 })
 export class StarredDirective {
-  @HostBinding('class.is-starred') isStarred = true;
+  @HostBinding('class.bi-star-fill') isStarred = true;
+  @HostBinding('class.bi-star') notStarred = true;
+
+
+  @Input() set atStarred(value){
+      this.isStarred = value;
+      this.notStarred =!value;
+  }
   constructor() { }
 
 }
